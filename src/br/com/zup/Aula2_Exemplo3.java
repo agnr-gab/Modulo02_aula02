7115fc291e70bc373acd91e2f7176ea9e1ba1155
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-24 15:11:18 -0300

    Inicio da lista de exercícios

---
+++ src/br/com/zup/Aula2_Exemplo3.java
@@ -0,0 +1,9 @@
+package br.com.zup;
+
+public class Aula2_Exemplo3 {
+    public static void main(String[] args) {
+        int quantidadeHorasTrabalhadas = 36;
+        float salarioHora = 6.91F;
+    }
+
+}
