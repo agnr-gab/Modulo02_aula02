5d80472a1e9bb7a9222794220155c8bb751af561
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-24 14:34:34 -0300

    Soma das variaveis

---
+++ src/br/com/zup/Aula2_Exemplo_2.java
@@ -0,0 +1,15 @@
+package br.com.zup;
+
+public class Aula2_Exemplo_2 {
+    public static void main(String[] args) {
+        //Armazenar os valores em memória
+        int numero1 = 6694;
+        int numero2 = 689;
+
+        //Criando a operação de soma
+        int resultadoDaSoma = numero1 + numero2;
+
+        //Exibindo o resultado da soma
+        System.out.println(resultadoDaSoma);
+    }
+}
