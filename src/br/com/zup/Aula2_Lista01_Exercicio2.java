1b35c3335a9293a88cb72f1a1f776f0e9bc74d78
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-24 15:58:00 -0300

    Média Bimestral

--- src/br/com/zup/Aula2_Lista01_Exercicio2.java
+++ src/br/com/zup/Aula2_Lista01_Exercicio2.java
@@ -3,15 +3,16 @@ package br.com.zup;
 public class Aula2_Lista01_Exercicio2 {
     public static void main(String[] args) {
         // 2 - Faça um programa que receba 4 notas bimestrais e mostre a média aritmética.
+
         float nota1 = 10F;
         float nota2 = 8.8F;
         float nota3 = 7.6F;
         float nota4 = 5.5F;
 
         float notasBimestres = nota1 + nota2 + nota3 + nota4;
-        float mediaBimestral = notasBimestres / 4;
+        float mediaBimestral = (notasBimestres) / 4;
 
-        System.out.println("Sua média é" + notasBimestres);
+        System.out.println("Sua média final é " + mediaBimestral);
     }
 
 }
