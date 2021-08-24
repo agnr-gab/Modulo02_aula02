5a6abb90594c02a3599345ffbef52917939b6ceb
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-24 14:14:22 -0300

    Inserindo concatenação

--- src/br/com/zup/Exemplo_1.java
+++ src/br/com/zup/Exemplo_1.java
@@ -6,11 +6,15 @@ public class Exemplo_1 {
         int idade_do_usuario = 28;
         String nomeDoUsuario = "Arê";
 
+        //Printando as variaveis declaradas
         System.out.print("Olá, meu nome é ");
         System.out.print(nomeDoUsuario);
         System.out.print(" e minha idade é ");
         System.out.print(idade_do_usuario);
-        System.out.print(" anos");
+        System.out.println(" anos");
+
+        //Printando de forma concatenada as variaveis declaradas
+        System.out.println("Olá, meu nome é " + nomeDoUsuario + " e minha idade é " + idade_do_usuario + " anos");
     }
 }
 
