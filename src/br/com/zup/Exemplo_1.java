43f1c2d2e04507d5f000fdff7d04ea6c00f74f8d
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-24 14:02:41 -0300

    Adicionando novas informações

--- src/br/com/zup/Exemplo_1.java
+++ src/br/com/zup/Exemplo_1.java
@@ -3,7 +3,14 @@ package br.com.zup;
 public class Exemplo_1 {
     public static void main(String[] args) {
         System.out.println("Aula 02 - Introdução ao JAVA");
+        int idade_do_usuario = 28;
+        String nomeDoUsuario = "Arê";
 
+        System.out.print("Olá, meu nome é ");
+        System.out.print(nomeDoUsuario);
+        System.out.print(" e minha idade é ");
+        System.out.print(idade_do_usuario);
+        System.out.print(" anos");
     }
 }
 
