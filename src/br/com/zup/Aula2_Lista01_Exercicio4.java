0aa505cb6a8ea394bd95c949752a2d57040ad9be
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-24 16:36:50 -0300

    Exercicio 05

--- src/br/com/zup/Aula2_Lista01_Exercicio4.java
+++ src/br/com/zup/Aula2_Lista01_Exercicio4.java
@@ -1,7 +1,9 @@
 package br.com.zup;
 
-public class Aula2_Exemplo3 {
+public class Aula2_Lista01_Exercicio4 {
     public static void main(String[] args) {
+       // 4 - Faça um Programa que recebe o quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
         float quantidadeHorasTrabalhadas = 450;
         float salarioHora = 6.91F;
 
@@ -10,6 +12,7 @@ public class Aula2_Exemplo3 {
 
         System.out.println("Você trabalhou " + quantidadeHorasTrabalhadas + "  Seu salário por hora é R$ " + salarioHora);
         System.out.println("Seu salário bruto será R$ " + salarioBruto + " reais");
+
     }
 
 }
---
+++ src/br/com/zup/Aula2_Lista01_Exercicio5.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+public class Aula2_Lista01_Exercicio5 {
+    public static void main(String[] args) {
+        // 5 - Faça um programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+
+       float temperaturaF = 96.8F;
+       float conversaoFToC = (temperaturaF − 32) * 5/9;
+        System.out.println("A");
+
+    }
+}
