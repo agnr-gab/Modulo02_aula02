1e562950f9cefcd685b656a300634f28ccbcea54
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-24 17:51:42 -0300

    Exercicios 06

--- src/br/com/zup/Aula2_Lista01_Exercicio6.java
+++ src/br/com/zup/Aula2_Lista01_Exercicio6.java
@@ -8,14 +8,25 @@ public class Aula2_Lista01_Exercicio6 {
         //c. Quanto pagou ao sindicato.
         //d. Qual o salário líquido do funcionário.
 
-        float quantidadeHorasTrabalhadas = 450;
+        float quantidadeHorasTrabalhadas = 450F;
         float salarioHora = 6.91F;
 
-        //Calcular o salário bruto
+        //Calculo do salário bruto
         float salarioBruto = quantidadeHorasTrabalhadas * salarioHora;
+        // Calculo INSS
+        float inss = salarioBruto * 0.08F;
+        // Calculo pagamento do sindicato
+        float sindicato = salarioBruto * 0.05F;
+        // Calculo imposto de renda
+        float impostoDeRenda = salarioBruto * 0.11F;
+
+
 
         System.out.println("Você trabalhou " + quantidadeHorasTrabalhadas + "  Seu salário por hora é R$ " + salarioHora);
         System.out.println("Seu salário bruto será R$ " + salarioBruto + " reais");
+        System.out.println("Seu salário sem impostos seria R$" + salario);
+        System.out.println("Foi descontado R$" + inss + " referente ao INSS e R$" + sindicato + " referente ao sindicato" );
+        System.out.println("Seu salário líquido será de R$" + salarioLiquido);
 
 
     }
