00acd1b9a9f5a7af55d41ef024ce32acdf5e4592
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-24 16:16:02 -0300

    m to cm

--- src/br/com/zup/Aula2_Lista01_Exercicio3.java
+++ src/br/com/zup/Aula2_Lista01_Exercicio3.java
@@ -1,2 +1,12 @@
-package br.com.zup;public class Aula2_Lista01_Exercicio3 {
+package br.com.zup;
+
+public class Aula2_Lista01_Exercicio3 {
+    public static void main(String[] args) {
+        // 3 - Faça um programa que converta metros para centímetros.
+
+        int medidaMetros = 1;
+        int resultadoMetrosParaCentimetros = medidaMetros * 100;
+        System.out.println("A conversão " + medidaMetros + "m para centimentros é de " + resultadoMetrosParaCentimetros + " cm");
+    }
+
 }
