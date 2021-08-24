d9ee898ba9b561eb61e847562a6d4067901cc858
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-24 15:44:36 -0300

    Olá mundo!

--- src/br/com/zup/Aula2_Lista01_Exercicio1.java
+++ src/br/com/zup/Aula2_Lista01_Exercicio1.java
@@ -1,4 +1,10 @@
 package br.com.zup;
 
 public class Aula2_Lista01_Exercicio1 {
+    public static void main(String[] args) {
+        //1 - Faça um programa que mostre a mensagem "Alô mundo" na tela.
+        System.out.println("Olá, Mundo! Hello World!");
+
+    }
+
 }
