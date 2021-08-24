5a13a83b6e0de46ec0cbea375b17cc2bcdd207c1
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-24 15:36:58 -0300

    Exercicio 01

--- src/br/com/zup/Aula2_Exemplo3.java
+++ src/br/com/zup/Aula2_Exemplo3.java
@@ -2,8 +2,14 @@ package br.com.zup;
 
 public class Aula2_Exemplo3 {
     public static void main(String[] args) {
-        int quantidadeHorasTrabalhadas = 36;
+        float quantidadeHorasTrabalhadas = 450;
         float salarioHora = 6.91F;
+
+        //Calcular o salário bruto
+        float salarioBruto = quantidadeHorasTrabalhadas * salarioHora;
+
+        System.out.println("Você trabalhou " + quantidadeHorasTrabalhadas + " horas essa semana. Seu salário por hora é R$ " + salarioHora);
+        System.out.println("Seu salário bruto será R$ " + salarioBruto );
     }
 
 }
---
+++ src/br/com/zup/Aula2_Lista01_Exercicio1.java
@@ -0,0 +1,4 @@
+package br.com.zup;
+
+public class Aula2_Lista01_Exercicio1 {
+}
